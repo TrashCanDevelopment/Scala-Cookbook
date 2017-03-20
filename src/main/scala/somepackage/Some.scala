3ae4cb6322da935bae3fd971254cd511ba570ee9
Vladimir Logachev <doit@keemail.me>
2017-03-21 01:46:49 +0700

    upd

--- src/main/scala/somepackage/Some.scala
+++ src/main/scala/somepackage/Some.scala
@@ -10,11 +10,37 @@ object SomethingWithMain {
     println("Arguments: "+args.mkString(" "))
     println(Cookbook)
 
+    // неявно вызывает unapply
     Cookbook.pbj match {
       case Recipe(ingredients, directions) =>
         println(ingredients)
         println(directions)
     }
+
+    // неявно вызывает unapply
+    def isSimple(recipe: Recipe): Boolean = recipe match {
+      case Recipe(_, List(_)) => true // проверяет на предмет инструкций «только из одной строки»
+      case _ => false
+    }
+    println(isSimple(Cookbook.pbj)) //true
+    println(isSimple(Cookbook.pancakes)) //true
+
+    // еще пример
+    object Even {
+      def unapply(n: Int): Option[Int] =
+        if (n % 2 == 0) {
+          Some(n / 2)
+        } else None
+    }
+    println(Even.unapply(4)) // Some (2)
+    println(Even.unapply(3)) // None
+    println(4 match {
+      case Even(half) => half
+    }) // 2
+    println(
+      3 match {
+        case Even(half) => half
+      }) // scala.MatchError
 
     for (i: Int <- 1 to 3) println(" ")
   }
