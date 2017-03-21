5c4632e0151bf2b961e610e5077a6d12f910a3d5
Vladimir Logachev <doit@keemail.me>
2017-03-21 14:02:54 +0700

    begin moving to Traits

--- src/main/scala/cookbook/Cookbook.scala
+++ src/main/scala/cookbook/Cookbook.scala
@@ -1,10 +1,18 @@
 package cookbook
 
-case class Recipe(ingredients: Map[String, Mass], directions: List[String])
+case class Recipe(ingredients: Map[String, Mass], directions: List[String]) {
+  def shoppingList(kitchen: Map[String, Mass]): List[String] =
+    for {
+      (name, need) <- ingredients.toList
+      have = kitchen.getOrElse(name, Grams(0))
+      if have < need
+    } yield name
+}
 
-abstract class Mass {
+sealed abstract class Mass extends Ordered[Mass] {
   def amount: Double
   def toGrams: Grams
+  def compare(that: Mass): Int = (this.toGrams.amount - that.toGrams.amount).toInt
 }
 
 case class Grams(amount: Double) extends Mass {
@@ -13,23 +21,23 @@ case class Grams(amount: Double) extends Mass {
 }
 
 case class Milligrams(amount: Double) extends Mass {
-  override def toGrams: Grams = new Grams(amount / 1000)
+  override def toGrams: Grams = Grams(amount / 1000)
   override def toString: String = amount + "mg"
 }
 
 case class Kilograms(amount: Double) extends Mass {
-  override def toGrams: Grams = new Grams(amount * 1000)
+  override def toGrams: Grams = Grams(amount * 1000)
   override def toString: String = amount + "kg"
 }
 
 object Cookbook {
   val pbj = Recipe(
-    Map("peanut butter" -> new Grams(10),
-      "jelly" -> new Kilograms(0.5),
-      "bread" -> new Grams(250)),
+    Map("peanut butter" -> Grams(10),
+      "jelly" -> Kilograms(0.5),
+      "bread" -> Grams(250)),
     List("just do it"))
   val pancakes = Recipe(
-    Map("bacon" -> new Kilograms(1),
-      "pancakes" -> new Miligrams(10)),
+    Map("bacon" -> Kilograms(1),
+      "pancakes" -> Milligrams(10)),
     List("just do it NOW!"))
 }
--- src/main/scala/somepackage/SomethingWithMain.scala
+++ src/main/scala/somepackage/SomethingWithMain.scala
@@ -8,7 +8,10 @@ object SomethingWithMain {
     for (i: Int <- 1 to 3) println(" ")
 
     println("Arguments: "+args.mkString(" "))
-    println(Cookbook)
+    println(Cookbook) //cookbook.Cookbook$@1d8c2de7
+
+    println(Cookbook.pbj.shoppingList(Map.empty)) // List(peanut butter, jelly, bread)
+    println(Cookbook.pbj.shoppingList(Map("peanut butter" -> Kilograms(1)))) // List(jelly, bread)
 
     // неявно вызывает unapply
     Cookbook.pbj match {
@@ -25,12 +28,10 @@ object SomethingWithMain {
     println(isSimple(Cookbook.pbj)) //true
     println(isSimple(Cookbook.pancakes)) //true
 
-    // еще пример
+    // еще пример c unapply
     object Even {
       def unapply(n: Int): Option[Int] =
-        if (n % 2 == 0) {
-          Some(n / 2)
-        } else None
+        if (n % 2 == 0) Some(n / 2) else None
     }
     println(Even.unapply(4)) // Some (2)
     println(Even.unapply(3)) // None
@@ -41,6 +42,14 @@ object SomethingWithMain {
 //      3 match {
 //        case Even(half) => half
 //      }) // scala.MatchError
+
+    def printMass(m: Mass) = m match {
+      case Grams(amount) => println(amount + " grams")
+      case _ => println("Other stuff. like return None.")
+    }
+    printMass(Grams(10))
+    printMass(Kilograms(10))
+    printMass(Milligrams(10))
 
     for (i: Int <- 1 to 3) println(" ")
   }
